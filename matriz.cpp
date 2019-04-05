03137703d75df9b5ac3c3015d390ccb61e83ac7f
JuanMonzon191202 <blackhorse7w7@gmail.com>
2019-04-04 22:49:20 -0600

    3er corte de juan

---
+++ matriz.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+int matriz[3][4]
+for(int i=0;i<=3;i++){
+   int j;
+   for(int j=0;j<=4;++)
+   {
+   cout<<"...",i," ",j,;
+   cin>>matriz[i][j]
+    }
+}	
+}
+
+
